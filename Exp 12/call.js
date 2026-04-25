9ebd42e321d428b8f6ec11e9612ffc20b36ffdca
ketkar-atharva <atharvaketkar13@gmail.com>
2026-04-25 16:29:14 +0530

    added exp 12

---
+++ Exp 12/call.js
@@ -0,0 +1,12 @@
+const square =(n)=> n*n;
+const cube=(n)=>n*n*n;
+
+function process(data , op){
+       return data.map(op);
+}
+
+function run(opp){
+    let data=[2,4,6,8];
+    let n=process(data,opp);
+    document.getElementById("ans").innerText=n;
+}
