a3184d8c50bee24c802a9aefe4f2765a4702f04b
ketkar-atharva <atharvaketkar13@gmail.com>
2026-04-26 11:57:45 +0530

    added exp 11

---
+++ Exp 11/captial.js
@@ -0,0 +1,13 @@
+
+let capitals={
+    "India":"Delhi",
+    "Japan":"Tokyo",
+    "UK":"London",
+    "USA":"Washington",
+};
+
+function show(){
+    let country=document.getElementById("country").value;
+    console.log(country);
+    document.getElementById("ans").innerHTML = capitals[country] ? `Captial : ${capitals[country]}`:"";
+}
